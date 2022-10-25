a4e32c8b7d9fe1c017657d78f5b5bb7841048a8b
Jojie Jagonos <jojie.jagonos23@gmail.com>
2022-10-25 17:49:40 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import './App.css';
+const App: FC = () => {
+  return (
+    <div className="App">
+      <h1>DOCKERIZE REACT TYPESCRIPT</h1>
+    </div>
+  );
+};
+
+export default App;
